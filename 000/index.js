73990331eaf40f4dc94c2d95ac93811cfbc4717b
SigitaZaromskiene <sigitazaromskiene@yahoo.com>
2023-02-07 11:30:01 +0200

    homework

--- 000/index.js
+++ 000/index.js
@@ -94,17 +94,140 @@ console.log("HOMEWORK");
 
 // 1
 
-function getRandomIntInclusive(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
-}
-const speeds = [];
-for (let i = 0; i < 10; i++) {
-  speeds.push(getRandomIntInclusive(120, 220));
+// function getRandomIntInclusive(min, max) {
+//   min = Math.ceil(min);
+//   max = Math.floor(max);
+//   return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+// }
+// const speeds = [];
+// for (let i = 0; i < 10; i++) {
+//   speeds.push(getRandomIntInclusive(120, 220));
 
-  console.log(speeds);
-}
-const average = speeds.reduce((a, b) => a + b) / 10;
+//   console.log(speeds);
+// }
+// const average = speeds.reduce((a, b) => a + b) / 10;
 
-console.log(average);
+// console.log(average);
+
+// 3
+
+// function wilBeEnough() {
+//   const isEnoughFuel = [];
+//   let sum;
+
+//   for (let i = 0; i < 10; i++) {
+//     function getRandomIntInclusive(min, max) {
+//       min = Math.ceil(min);
+//       max = Math.floor(max);
+//       return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+//     }
+
+//     isEnoughFuel.push(getRandomIntInclusive(3, 6));
+//   }
+//   console.log(isEnoughFuel);
+//   sum = isEnoughFuel.reduce((acc, num) => acc + num);
+//   console.log(sum);
+
+//   console.log(44 - sum > 0 ? "Enough" : "Not enougth");
+// }
+
+// wilBeEnough();
+
+// 4
+
+// const loops = [];
+// for (let i = 0; i < 10; i++) {
+//   function getRandomIntInclusive(min, max) {
+//     min = Math.ceil(min);
+//     max = Math.floor(max);
+//     return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+//   }
+//   loops.push(getRandomIntInclusive(20, 120));
+// }
+
+// const leftRight = [];
+// for (let i = 0; i < 5; i++) {
+//   function getRandomIntInclusive(min, max) {
+//     min = Math.ceil(min);
+//     max = Math.floor(max);
+//     return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+//   }
+//   leftRight.push(getRandomIntInclusive(20, 120));
+// }
+
+// console.log(loops, leftRight);
+
+// const min = Math.min(...leftRight);
+// console.log(min);
+
+// 5
+
+// const stabdziai = rand(0, 1);
+// const vairas = rand(0, 1);
+// const kengura = rand(0, 1);
+// console.log(stabdziai, vairas, kengura);
+
+// function rand(min, max) {
+//   min = Math.ceil(min);
+//   max = Math.floor(max);
+//   return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+// }
+
+// let km = 0;
+// do {
+//   km + 1;
+// } while (stabdziai == true);
+// console.log(km);
+
+//  3333333333333333333
+
+const bitGirls = ["Edita", "Lina", "Brigita", "Deimante", "Juste"];
+const cats = ["Murka", "Rainius", "Meilute", "Bosas", "Dickis"];
+
+// 1
+
+bitGirls.unshift("Nauseda");
+console.log(bitGirls);
+
+const bitCats = [];
+
+for (let i = 0; i < cats.length; i++) {
+  const or = ["Storas", "Nestoras"];
+  const fatOrNot = or[Math.floor(Math.random() * or.length)];
+
+  bitCats.push([cats[i], fatOrNot]);
+
+  const includes = bitCats.filter((a) => a.includes("Storas"));
+  console.log(includes.length);
+}
+
+// const sorted = bitCats.sort(function (a, b) {
+//   return a[1].localeCompare(b[1]);
+// });
+
+// console.log(sorted);
+
+// const sorted = bitCats.sort(function (a, b) {
+//   if (a[1] < b[1]) return -1;
+//   if (a[1] > b[1]) return 1;
+//   return 0;
+// });
+
+// console.log(sorted);
+
+// const sorted = bitCats.sort((a, b) => a[1] - b[1]);
+
+// console.log(sorted);
+
+// 5
+
+// const happyCats = [];
+
+// happyCats.push([bitGirls[0], "Barsukas"]);
+
+// for (let i = 0; i < bitCats.length; i++) {
+//   const [catName] = bitCats[i];
+//   happyCats.push([bitGirls[i + 1], catName]);
+// }
+
+// console.log(happyCats);
